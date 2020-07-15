300221230936a6c0315d7a917621b4606748f1f2
Eric Marty <contact@ericmarty.co>
2020-07-15 13:09:02 -0700

    Move layout html out of component, into page template.
    And rename the componenet to EditPermissionsButton.

---
+++ resources/js/components/EditPermissionsButton.vue
@@ -0,0 +1,11 @@
+<template>
+    <a href="#" class="btn btn-primary"><i class="far fa-edit"></i></a>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
--- resources/views/apexspa.blade.php
+++ resources/views/apexspa.blade.php
@@ -4,6 +4,32 @@
 
 @section('content')
     <div id="app">
-        <main-button></main-button>
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-md-8">
+                    <div class="card">
+                        <div class="card-header">Users</div>
+                        <div class="card-body">
+                            <table class="table">
+                                <thead>
+                                  <tr>
+                                    <th>Name</th>
+                                    <th>Role</th>
+                                    <th>Permission</th>
+                                  </tr>
+                                </thead>
+                                <tbody>
+                                  <tr>
+                                    <td>Eric</td>
+                                    <td>Developer</td>
+                                    <td><edit-permissions-button></edit-permissions-button></td>
+                                  </tr>
+                                </tbody>
+                              </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 @endsection
