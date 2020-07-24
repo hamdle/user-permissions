8c724046df65d29c3a0373f44d44c6c1b35b9e27
Eric Marty <contact@ericmarty.co>
2020-07-24 14:52:53 -0700

    Shift around contents of vue components.

--- resources/js/components/EditPermissionsAccordion.vue
+++ resources/js/components/EditPermissionsAccordion.vue
@@ -1,61 +1,13 @@
 <template>
-  <div role="tablist">
-    <b-card no-body class="mb-1">
-      <b-card-header header-tag="header" class="p-1" role="tab">
-        <b-button block v-b-toggle.accordion-1 variant="info" class="text-left"><i class="fas fa-users"></i>Company Permissions</b-button>
-      </b-card-header>
-      <b-collapse id="accordion-1" visible accordion="my-accordion" role="tabpanel">
-        <b-card-body>
-          <b-card-text>I start opened because <code>visible</code> is <code>true</code></b-card-text>
-          <b-card-text>
-                <b-form-checkbox
-                      id="checkbox-1"
-                      v-model="status"
-                      name="checkbox-1"
-                      value="accepted"
-                      size="xl"
-                      unchecked-value="not_accepted"
-                >
-      I accept the terms and use
+    <b-form-checkbox
+          id="checkbox-1"
+          v-model="status"
+          name="checkbox-1"
+          value="accepted"
+          size="xl"
+          unchecked-value="not_accepted"
+    >
     </b-form-checkbox>
-          </b-card-text>
-        </b-card-body>
-      </b-collapse>
-    </b-card>
-
-    <b-card no-body class="mb-1">
-      <b-card-header header-tag="header" class="p-1" role="tab">
-        <b-button block v-b-toggle.accordion-2 variant="info" class="text-left"><i class="fas fa-cart-plus"></i>Buyer Permissions</b-button>
-      </b-card-header>
-      <b-collapse id="accordion-2" accordion="my-accordion" role="tabpanel">
-        <b-card-body>
-          <b-card-text>{{ text }}</b-card-text>
-        </b-card-body>
-      </b-collapse>
-    </b-card>
-
-    <b-card no-body class="mb-1">
-      <b-card-header header-tag="header" class="p-1" role="tab">
-        <b-button block v-b-toggle.accordion-3 variant="info" class="text-left"><i class="fas fa-search-dollar"></i>Seller Permissions</b-button>
-      </b-card-header>
-      <b-collapse id="accordion-3" accordion="my-accordion" role="tabpanel">
-        <b-card-body>
-          <b-card-text>{{ text }}</b-card-text>
-        </b-card-body>
-      </b-collapse>
-    </b-card>
-
-    <b-card no-body class="mb-1">
-      <b-card-header header-tag="header" class="p-1" role="tab">
-        <b-button block v-b-toggle.accordion-4 variant="info" class="text-left"><i class="fas fa-paste"></i>General Permissions</b-button>
-      </b-card-header>
-      <b-collapse id="accordion-4" accordion="my-accordion" role="tabpanel">
-        <b-card-body>
-          <b-card-text>{{ text }}</b-card-text>
-        </b-card-body>
-      </b-collapse>
-    </b-card>
-  </div>
 </template>
 
 <script>
--- resources/js/components/EditPermissionsModal.vue
+++ resources/js/components/EditPermissionsModal.vue
@@ -1,6 +1,19 @@
 <template>
     <b-modal id="modal-1" size="xl" title="BootstrapVue" hide-footer>
-        <edit-permissions-accordion></edit-permissions-accordion>
+        <div role="tablist">
+            <b-card no-body class="mb-1">
+                <b-card-header header-tag="header" class="p-1" role="tab">
+                    <b-button block v-b-toggle.accordion-1 variant="info" class="text-left"><i class="fas fa-users"></i>Company Permissions</b-button>
+                </b-card-header>
+                <b-collapse id="accordion-1" visible accordion="my-accordion" role="tabpanel">
+                    <b-card-body>
+                        <b-card-text>
+                            <edit-permissions-accordion></edit-permissions-accordion>
+                        </b-card-text>
+                    </b-card-body>
+                </b-collapse>
+            </b-card>
+        </div>
     </b-modal>
 </template>
 
